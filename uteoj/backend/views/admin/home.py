b524ec601c334918e48412bf202c02d6291d5064
ngthvan1612 <ngthvan1612@gmail.com>
2021-06-29 16:39:24 +0700

    Update home.py

--- uteoj/backend/views/admin/home.py
+++ uteoj/backend/views/admin/home.py
@@ -3,7 +3,25 @@ from django.shortcuts import render
 
 
 def AdminHomeView(request):
+    list_problems = [
+        {
+            'id': 1,
+            'shortname': 'SUMAB',
+            'fullname': 'Tinh tong 2 so nguyen AB'
+        },
+        {
+            'id': 2,
+            'shortname': 'MULAB',
+            'fullname': 'Tinh tich 2 so nguyen AB'
+        },
+        {
+            'id': 3,
+            'shortname': 'POWERAB',
+            'fullname': 'Mu 2 so nguyen AB'
+        },
+    ]
     context = {
         'website_header_title': 'Trang chu',
+        'list_problems': list_problems,
     }
     return render(request, 'admin-template/index.html', context)
