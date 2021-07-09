259a1888fcbfc51b1520da64a44d464343ee785a
Kai-BignumD <58112725+Kai-BignumD@users.noreply.github.com>
2021-07-09 07:45:29 -0700

    Update Modal

--- uteoj/frontend/statics/category/js/index.js
+++ uteoj/frontend/statics/category/js/index.js
@@ -1,5 +1,6 @@
 const triggerTabList = [].slice.call(document.querySelectorAll('#list-tab a'))
 const triggerDivList = [].slice.call(document.querySelectorAll('#nav-tabContent div'))
+
 triggerTabList[0].classList.add("active")
 triggerDivList[0].classList.add("show", "active")
 
