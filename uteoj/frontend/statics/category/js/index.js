d9ecf09712939f5de8305d49d1a6305e32583e42
Kai-BignumD <58112725+Kai-BignumD@users.noreply.github.com>
2021-07-10 00:33:52 -0700

    Update Category Filter

--- uteoj/frontend/statics/category/js/index.js
+++ uteoj/frontend/statics/category/js/index.js
@@ -1,5 +1,6 @@
 var triggerTabList = [].slice.call(document.querySelectorAll('#list-tab a'))
 var triggerDivList = [].slice.call(document.querySelectorAll('#nav-tabContent div'))
+var test = document.getElementsByClassName("active");
 var inputName = document.getElementById("inputName");
 var description = document.getElementById("description");
 
@@ -15,26 +16,31 @@ triggerTabList.forEach(function (triggerEl) {
   })
 })
 
-function addNewCategory(){
+function addNewCategory() {
   $.ajax({
     url: '/admin/categories/',
     type: 'POST',
     data: {
-      method : 'add', //method là add
-      name : inputName.value, //cái tên mới
-      description : description.value, //thông tin phụ
+      method: 'add', //method là add
+      name: inputName.value, //cái tên mới
+      description: description.value, //thông tin phụ
       csrfmiddlewaretoken: CSRF_TOKEN, ///phải có (ở trên)
     },
-    success: function(data) {
-        if (data.status=='success') {
-            ///xử lý message khi thành công - có thể alert ra, có thể cập nhật vào label trong modal (đổi thành màu xanh)
-            console.log("SUCCESS: " + data.message);
-            reloadCurrentSource();
-        }
-        else {
-            ///xử lý message khi lỗi (như trên)
-            console.log("ERROR: " + data.message);
-        }
+    success: function (data) {
+      if (data.status == 'success') {
+        ///xử lý message khi thành công - có thể alert ra, có thể cập nhật vào label trong modal (đổi thành màu xanh)
+        console.log("SUCCESS: " + data.message);
+        reloadCurrentSource();
+      } else {
+        ///xử lý message khi lỗi (như trên)
+        console.log("ERROR: " + data.message);
+      }
     }
-})
+  })
+}
+
+function showProblem(){
+  var value = test[1].id;
+  console.log(value);
+  window.location= "http://127.0.0.1:8000/admin/problems/?category="+value;
 }
