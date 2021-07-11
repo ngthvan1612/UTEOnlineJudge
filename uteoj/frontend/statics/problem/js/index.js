4a1657aa29bcb949e65ca185b52bfec8ee7917fa
Kai-BignumD <58112725+Kai-BignumD@users.noreply.github.com>
2021-07-11 08:20:28 -0700

    Update Problem Filter

--- uteoj/frontend/statics/problem/js/index.js
+++ uteoj/frontend/statics/problem/js/index.js
@@ -27,3 +27,51 @@ function search_table() {
         }
     }
 }
+
+function showSelect(){
+    var item = document.getElementById("addCatetory");
+    var selectItem = document.getElementById("selectCategory");
+    console.log(selectItem);
+    item.style.display="none";
+    selectItem.style.display="block";
+}
+
+function chooseCategory(){
+    var checkExist = false;
+    var categoryBoxes = document.getElementsByClassName("listCatgories")[0];
+    var categoryAdds = document.getElementsByClassName("categoryAdd");
+    var selectBox = document.getElementById("selectCategory");
+
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+
+    var newBox = document.createElement("div");
+    var icon = document.createElement("i");
+    var h5 = document.createElement("span");
+
+    newBox.id = selectedValue;
+
+    h5.textContent = selectedValue;
+    h5.style.fontSize = "14px";
+
+    icon.classList.add("fa","fa-times");
+    icon.style.marginLeft = "6px";
+    icon.style.fontSize = "14px";
+    icon.id = selectedValue;
+    icon.onclick=function(){
+        var element = document.getElementById(this.id);
+        categoryBoxes.removeChild(element);
+    }
+
+    newBox.classList.add("btn", "btn-secondary","categoryAdd");
+    newBox.name = selectedValue;
+    newBox.appendChild(h5);
+    newBox.appendChild(icon);
+    newBox.style.display = "flexbox";
+    newBox.style.marginRight = "3px";
+    newBox.style.marginTop = "3px";
+
+    for (var i=0;i<categoryAdds.length;i++) 
+        if (categoryAdds[i].name == selectedValue) checkExist=true;
+    
+    if (!checkExist && selectedValue!="") categoryBoxes.appendChild(newBox);
+}
