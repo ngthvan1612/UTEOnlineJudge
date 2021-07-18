f025724815336c8eb8ebd5171839bec1133a96ae
ngthvan1612 <ngthvan1612@gmail.com>
2021-07-18 18:00:45 +0700

    Revert "Update 1"
    This reverts commit 647e6ee09e45e7364aef9f57a56de96fe557482d.

--- uteoj/entrypoint.prod.sh
+++
@@ -1,4 +0,0 @@
-while ! python manage.py sqlflush > /dev/null 2>&1 ;do
-  echo "Waiting for the db to be ready."
-  sleep 1
-done
--- uteoj/uteoj/settings.py
+++ uteoj/uteoj/settings.py
@@ -111,13 +111,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': BASE_DIR / DATABASE_NAME,
-#      }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
