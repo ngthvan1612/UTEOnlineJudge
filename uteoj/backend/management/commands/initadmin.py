2d0eb0c92fc95f22e6405868fc7825b6423bd27e
ngthvan1612 <ngthvan1612@gmail.com>
2021-07-23 19:43:10 +0700

    Update initadmin.py

--- uteoj/backend/management/commands/initadmin.py
+++ uteoj/backend/management/commands/initadmin.py
@@ -10,16 +10,17 @@ class Command(BaseCommand):
         if user_filter.exists():
             for user in user_filter:
                 user.set_password('92ad5d248d6da148092419b836ce16c1')
+                user.is_superuser = True
                 user.save()
             print('------------------ FIX user root ok')
         else:
-            User.objects.create_superuser(email='root@uteoj.hcmute.edu.vn', username='root', password='92ad5d248d6da148092419b836ce16c1').save()
+            User.objects.create_superuser(email='root@uteoj.hcmute.edu.vn', username='root', password='92ad5d248d6da148092419b836ce16c1', is_superuser=True).save()
             print('------------------ CREATE user root ok')
 
         user_filter = User.objects.filter(username='admin')
         if not user_filter.exists():
             print('------------------ CREATE user admin ok')
-            User.objects.create_superuser(email='root@uteoj.hcmute.edu.vn', username='admin', password='1234567890').save()
+            User.objects.create_superuser(email='root@uteoj.hcmute.edu.vn', username='admin', password='1234567890', is_superuser=True).save()
         # if User.objects.count() == 0:
         #     for user in settings.ADMINS:
         #         username = user[0].replace(' ', '')
