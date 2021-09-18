7b6f68abd612a61f0992b4367ed36a654faf7f26
Kai-BignumD <58112725+Kai-BignumD@users.noreply.github.com>
2021-09-18 19:24:03 +0700

    Update and Fix bug

---
+++ HowToRun.md
@@ -0,0 +1,2 @@
+1. Phải chạy lệnh này trước: redis-server --daemonize yes (chạy 1 lần)
+2. Chạy chấm bài: celery -A uteoj worker -Ofair
--- uteoj/frontend/statics/user-static/js/problem/submit.js
+++ uteoj/frontend/statics/user-static/js/problem/submit.js
@@ -3,6 +3,12 @@ editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/c_cpp");
 
 function check(){
-    var valueIn = document.getElementsByClassName("ace_text-input")[0].value;
-    document.getElementById("source").value = valueIn;
+    var valueIn = document.getElementsByClassName("ace_line");
+    var data = "";
+    for (var i=0; i<valueIn.length; i++){
+        data+=(valueIn[i].children[0].textContent + "\n")
+    }
+
+
+    document.getElementById("source").value = data;
 }
