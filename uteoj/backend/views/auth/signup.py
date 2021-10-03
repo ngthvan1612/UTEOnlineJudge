14bce469de3149ed4c29b3947b1d5a81862d4179
ngthvan1612 <ngthvan1612@gmail.com>
2021-10-03 19:38:41 +0700

    Update signup.py

--- uteoj/backend/views/auth/signup.py
+++ uteoj/backend/views/auth/signup.py
@@ -62,7 +62,7 @@ def SignupView(request):
                 email_template = 'auth-template/verifyEmail.txt'
                 c = {
                     "email": user.email,
-                    "domain": "127.0.0.1:8000",
+                    "domain": "127.0.0.1:" + str(request.get_port()),
                     "site_name": "UTE ONLINE JUDGE",
                     "uid": urlsafe_base64_encode(force_bytes(user.pk)),
                     "user": "user",
